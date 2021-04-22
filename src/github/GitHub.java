92f976dd39ec28838c2dc58d8ad219d4222da59a
pollo <pollo@LAPTOP-5L0VIQQA.mshome.net>
2021-04-22 12:12:33 -0600

    Programa saluda

--- src/github/GitHub.java
+++ src/github/GitHub.java
@@ -7,6 +7,7 @@ public class GitHub {
     
     public static void main(String[] args) {
         System.out.println("Hola, soy Rubiuh");
+        System.out.println("Me gustan las hamburguesas");
     }
     
 }
